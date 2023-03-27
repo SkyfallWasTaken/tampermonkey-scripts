143b36949525bbd0288803be731de12c2469386e
SkyfallWasTaken <55807755+SkyfallWasTaken@users.noreply.github.com>
2023-03-27 18:23:46 +0100

    Create timerBypass.js

---
+++ gixen.com/timerBypass.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Gixen Timer Bypass
+// @namespace    http://gixen.com/
+// @version      0.2
+// @description  Bypasses the Gixen timer
+// @author       You
+// @match        https://www.gixen.com/main/home_2.php?sessionid=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gixen.com
+// @grant        none
+// @license      GPL-3.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let buttons = document.querySelectorAll("form#g_id input#gbutton");
+    let button = buttons[0];
+
+    // Sanity check
+    if (buttons.length === 1) {
+        // The button is the right one. Undisable the button and click it.
+        button.disabled = false;
+        button.click();
+    }
+})();
